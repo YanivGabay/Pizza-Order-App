a2a0576dae0d787e358548a190ffa14e683f76cc
Yaniv Gabay <yanivga@std.hac.ac.il>
2024-05-31 01:13:31 +0300

    basic controler and service, the /api/ingredients is working (checked with postman)

--- PizzaOrderApp/src/main/java/org/example/pizzaorderapp/controller/IngredientController.java
+++ PizzaOrderApp/src/main/java/org/example/pizzaorderapp/controller/IngredientController.java
@@ -1,4 +1,25 @@
 package org.example.pizzaorderapp.controller;
 
+import org.example.pizzaorderapp.model.Ingredient;
+import org.example.pizzaorderapp.service.IngredientService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/ingredients")
 public class IngredientController {
+
+    private final IngredientService ingredientService;
+
+    // Constructor for dependency injection
+    public IngredientController(IngredientService ingredientService) {
+        this.ingredientService = ingredientService;
+    }
+
+    @GetMapping
+    public List<Ingredient> getAllIngredients() {
+        return ingredientService.getAllIngredients();
+    }
 }
--- PizzaOrderApp/src/main/java/org/example/pizzaorderapp/service/IngredientService.java
+++ PizzaOrderApp/src/main/java/org/example/pizzaorderapp/service/IngredientService.java
@@ -1,4 +1,24 @@
 package org.example.pizzaorderapp.service;
 
+import org.example.pizzaorderapp.model.Ingredient;
+import org.springframework.stereotype.Service;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class IngredientService {
+    private final List<Ingredient> ingredients = new ArrayList<>();
+
+    public IngredientService() {
+        ingredients.add(new Ingredient(1L, "Tomato", "images/tomato.png"));
+        ingredients.add(new Ingredient(2L, "Cheese", "images/cheese.png"));
+        ingredients.add(new Ingredient(3L, "Peperoni", "images/peperoni.png"));
+        ingredients.add(new Ingredient(4L, "Sauce", "images/sauce.png"));
+        ingredients.add(new Ingredient(5L, "Ricotta", "images/ricotta.png"));
+        ingredients.add(new Ingredient(6L, "Olives", "images/olives.png"));
+    }
+
+    public List<Ingredient> getAllIngredients() {
+        return ingredients;
+    }
 }
