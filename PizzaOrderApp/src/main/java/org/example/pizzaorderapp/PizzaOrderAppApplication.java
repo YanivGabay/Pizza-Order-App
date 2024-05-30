f5e7e78c3e5146de12235b9fd5a20140bdec697b
Yaniv Gabay <yanivga@std.hac.ac.il>
2024-05-31 00:45:44 +0300

    Created a blank springProject

---
+++ PizzaOrderApp/src/main/java/org/example/pizzaorderapp/PizzaOrderAppApplication.java
@@ -0,0 +1,13 @@
+package org.example.pizzaorderapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaOrderAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaOrderAppApplication.class, args);
+	}
+
+}
---
+++ PizzaOrderApp/src/test/java/org/example/pizzaorderapp/PizzaOrderAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.pizzaorderapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaOrderAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
